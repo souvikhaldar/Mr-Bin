c7b32af1b356bf301c17a07163b6fe59a0c00a30
souvikhaldar <souvikhaldar32@gmail.com>
2018-05-09 18:45:05 +0530

    addPercentage function added

--- main.go
+++ main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"bytes"
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
+
+type DistanceStruct struct {
+	distance float64
+}
 
 var (
 	repeat int
@@ -25,6 +30,38 @@ func repeatFunc(c *gin.Context) {
 		buffer.WriteString("Hello from Go!")
 	}
 	c.String(http.StatusOK, buffer.String())
+}
+
+// this function is for putting the data from arduino to the server
+func addPercentage(c *gin.Context) {
+	height := 45.0
+	data, er := ioutil.ReadAll(c.Request.Body)
+	if er != nil {
+		fmt.Println("Error in reading from request body", er)
+		c.JSON(500, gin.H{"Error": "Reading"})
+		return
+	}
+	var dist DistanceStruct
+	oops := json.Unmarshal(data, &dist)
+	if oops != nil {
+		fmt.Println("Error in unmarshalling", oops)
+		c.JSON(500, gin.H{"error": "unmarshalling failed"})
+		return
+	}
+	fmt.Println("The recieved distance is ", dist.distance)
+	percentvalue := (dist.distance / height) * 100
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS percentage (id bigserial,percent int)"); err != nil {
+		fmt.Println("Error in creating percentage table", err)
+		c.JSON(500, gin.H{"error": "table creation failed"})
+		return
+	}
+	if _, err := db.Exec("INSERT INTO percentage(id,percent) VALUES (DEFAULT,%s", percentvalue); err != nil {
+		fmt.Println("Error in inserting percentage table", err)
+		c.JSON(500, gin.H{"error": "table insertion failed"})
+		return
+	}
+	c.JSON(200, gin.H{"status": "insert success"})
+	return
 }
 
 func dbFunc(c *gin.Context) {
@@ -67,12 +104,12 @@ func main() {
 	}
 
 	var err error
-	tStr := os.Getenv("REPEAT")
-	repeat, err = strconv.Atoi(tStr)
-	if err != nil {
-		log.Print("Error converting $REPEAT to an int: %q - Using default", err)
-		repeat = 5
-	}
+	//tStr := os.Getenv("REPEAT")
+	//repeat, err = strconv.Atoi(tStr)
+	//if err != nil {
+	//	log.Print("Error converting $REPEAT to an int: %q - Using default", err)
+	//	repeat = 5
+	//}
 
 	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
@@ -90,6 +127,7 @@ func main() {
 
 	router.GET("/repeat", repeatFunc)
 	router.GET("/db", dbFunc)
+	router.PUT("/put", addPercentage)
 
 	router.Run(":" + port)
 }
